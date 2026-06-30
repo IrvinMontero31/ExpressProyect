c3d06e3da593b9f7daeb78d7d1ac90819f742591
IrvinMontero31 <monterorvc@gmail.com>
2026-06-30 12:05:41 -0600

    Cambio de nombre3

--- routes/users.js
+++ routes/users.js
@@ -4,7 +4,7 @@ const router = express.Router();
 // Almacenamiento temporal en memoria
 let users = [
   { id: 1, usuario: 'irvin',   password: 'admin123',  rol: 'administrador' },
-  { id: 2, usuario: 'jlopez',  password: 'pass456',   rol: 'editor' },
+  { id: 2, usuario: 'julian',  password: 'pass456',   rol: 'editor' },
   { id: 3, usuario: 'mgarcia', password: 'secret789', rol: 'lector' },
 ];
 
